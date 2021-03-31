42ccf82d547f143bfc40a235fdbbe0f50b706e0e
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2021-03-31 10:16:22 +0200

    First commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+        </div>
+    </div>
+</div>
+@endsection
